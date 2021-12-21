abaeb42c3464ba5a7c2fa05130fa8502acabe795
NaanPradeep <58743127+NaanPradeep@users.noreply.github.com>
2021-12-21 14:03:33 +0530

    Update cardanocli.js

--- cardanocli.js
+++ cardanocli.js
@@ -2,14 +2,10 @@ const os = require("os");
 const path = require("path");
 const fetch = require("sync-fetch");
 const CardanocliJs = require("cardanocli-js");
-
 const dir = path.join(os.homedir(), "testnet");
-console.log(path.join(dir,"files","testnet-shelley-genesis.json"))
-
 exports.cardanocliJs = new CardanocliJs({
-  era: "mary", //current era
   network: "testnet-magic 1097911063",
   dir: dir, // working directory,
-  socketPath: path.join(dir,"db","socket"),
-  shelleyGenesisPath: path.join(dir,"files","testnet-shelley-genesis.json")
+  socketPath: path.join(dir,"cardano","db", "node.socket"),
+  shelleyGenesisPath: path.join(dir,"cardano","testnet-shelley-genesis.json")
 });
