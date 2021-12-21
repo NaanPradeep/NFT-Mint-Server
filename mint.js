a2c9fe404f0f5d7fb99ecc7ea4391251635fde1c
NaanPradeep <58743127+NaanPradeep@users.noreply.github.com>
2021-12-21 14:09:58 +0530

    Update mint.js

--- mint.js
+++ mint.js
@@ -1,17 +1,16 @@
 const { cardanocliJs } = require("./cardanocli");
 const fetch = require("node-fetch");
 const metadata = require("./metadata.json");
-
+const execSync = require('child_process').execSync;
 const createTransaction = (tx) => {
   let raw = cardanocliJs.transactionBuildRaw(tx);
   let fee = cardanocliJs.transactionCalculateMinFee({
     ...tx,
     txBody: raw,
   });
-  tx.txOut[0].amount.lovelace -= fee;
+  tx.txOut[0].value.lovelace -= fee;
   return cardanocliJs.transactionBuildRaw({ ...tx, fee });
 };
-
 const signTransaction = (wallet, tx, script) => {
   return cardanocliJs.transactionSign({
     signingKeys: [wallet.payment.skey, wallet.payment.skey],
@@ -19,102 +18,78 @@ const signTransaction = (wallet, tx, script) => {
     txBody: tx,
   });
 };
-
 const signTransactionRefund = (wallet, tx) => {
   return cardanocliJs.transactionSign({
     signingKeys: [wallet.payment.skey],
     txBody: tx,
   });
 };
-
 exports.initateMint = async (_utxo, id) => {
   const wallet = cardanocliJs.wallet("Test");
-
   const mintScript = {
     type: "all",
     scripts: [
-      { slot: 21560175 + 632000, type: "before" },
+      { slot: 45700368 + 632000, type: "before" },
       {
         keyHash: cardanocliJs.addressKeyHash(wallet.name),
         type: "sig",
       },
     ],
   };
-
   const utxo = JSON.parse(JSON.stringify(_utxo));
-
   const oldTxHash = utxo.txHash;
-
   const receiver = await fetch(
     `https://cardano-testnet.blockfrost.io/api/v0/txs/${oldTxHash}/utxos`,
-    { headers: { project_id: "rbkrp5hOr3khPAWNo3x47t6CP7qKFyA5" } }
+    { headers: { project_id: "testnet5APdWV3eEMTRg3v1vpB0V4PDoRGOAdWH" } }
   )
-    .then((res) => res.json())
+  .then((res) => res.json())
     .then((res) => res.inputs[0].address);
-
   const policy = cardanocliJs.transactionPolicyid(mintScript);
-  const name = `SpaceBud${id}`;
-  const SPACEBUD = policy + `.${name}`;
-  const balance = utxo.amount;
+  const name = `CoCatz${id}`;
+  const encodedName = execSync(`echo -n ${name} | xxd -ps`, { encoding: 'utf-8' });
+  const COCATZ = policy + `.${encodedName}`;
+  const balance = utxo.value;
   balance.lovelace -= cardanocliJs.toLovelace(1.5);
-  const [ziegAmount, alesAmount] = [
-    balance.lovelace - Math.floor(balance.lovelace / 2),
-    Math.floor(balance.lovelace / 2),
-  ];
-
   const tx = {
     txIn: [utxo],
     txOut: [
       {
         address:
-          "addr_test1qzuyhmjlv2qcg5rrk9hxp9hen595m7h8zs4h5yfz743k86gjx0wny5qwycfv0qp94vml3dqp3590v4f4xv2xym02eypqjc2430",
-        amount: { lovelace: alesAmount },
-      },
-      {
-        address:
-          "addr_test1qrez968pdxa5p55jpq4apn7uxv0msdeq8fa8x5su54s9k4gjx0wny5qwycfv0qp94vml3dqp3590v4f4xv2xym02eypqwleec8",
-        amount: { lovelace: ziegAmount },
+          "addr_test1qz6avr9pvt8a29625pvt7cgf9453rh0tvll0ne4fkmhtu4vtntrjh6lz39d9rcc8wakk5qm0gelnxr20zpag473z7tmqanrnzs",
+        value: { lovelace: balance.lovelace },
       },
       {
         address: receiver,
-        amount: {
-          ...balance,
-          [SPACEBUD]: 1,
+        value: {
+          [COCATZ]: 1,
           lovelace: cardanocliJs.toLovelace(1.5),
         },
       },
     ],
-    mint: [{ action: "mint", amount: 1, token: SPACEBUD }],
+    mint: [{ action: "mint", quantity: 1, asset: COCATZ, script: mintScript}],
     metadata: { 721: { [policy]: { [name]: metadata[id] } } },
     witnessCount: 2,
   };
-
   const raw = createTransaction(tx);
   const signed = signTransaction(wallet, raw, mintScript);
   const txHash = cardanocliJs.transactionSubmit(signed);
   return txHash;
 };
-
 exports.initateRefund = async (_utxo) => {
   const wallet = cardanocliJs.wallet("Test");
   const mintScript = {
     keyHash: cardanocliJs.addressKeyHash(wallet.name),
     type: "sig",
   };
-
   const utxo = JSON.parse(JSON.stringify(_utxo));
-
   const oldTxHash = utxo.txHash;
-
   const receiver = await fetch(
     `https://cardano-testnet.blockfrost.io/api/v0/txs/${oldTxHash}/utxos`,
-    { headers: { project_id: "rbkrp5hOr3khPAWNo3x47t6CP7qKFyA5" } }
+    { headers: { project_id: "testnet5APdWV3eEMTRg3v1vpB0V4PDoRGOAdWH" } }
   )
     .then((res) => res.json())
     .then((res) => res.inputs[0].address);
-
   const balance = utxo.amount;
-
   const tx = {
     txIn: [utxo],
     txOut: [
@@ -127,11 +102,10 @@ exports.initateRefund = async (_utxo) => {
     metadata: {
       0: {
         message:
-          "Refund. Amount matches no SpaceBud. Please send the exact amount",
+          "Refund. Amount matches no CoCatz. Please send the exact amount",
       },
     },
   };
-
   const raw = createTransaction(tx);
   const signed = signTransactionRefund(wallet, raw, mintScript);
   const txHash = cardanocliJs.transactionSubmit(signed);
